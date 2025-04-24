f9c70b93188f99131b0cf1c173a6054b41198197
Germano Ribeiro <germanno.verde@gmail.com>
2025-04-24 12:28:10 -0300

    anada

---
+++ Project-BTG-Pactual-Api/Applicacao/Consumers/OrderConsumer.cs
@@ -0,0 +1,16 @@
+﻿
+using MassTransit;
+using Project_BTG_Pactual_Api.Messages;
+using Project_BTG_Pactual_Api.Applicacao.interfacesServices;
+
+namespace Project_BTG_Pactual_Api.Applicacao.Consumers
+{
+    public class OrderConsumer : IConsumer<OrderMessage>
+    {
+        private readonly IOrderService _service;
+        public OrderConsumer(IOrderService service) => _service = service;
+
+        public Task Consume(ConsumeContext<OrderMessage> context) =>
+            _service.ProcessOrderAsync(context.Message);
+    }
+}
